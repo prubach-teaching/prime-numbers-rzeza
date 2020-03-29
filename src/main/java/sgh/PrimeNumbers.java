ec821af983c8c630095365fe8dd0554d9010908f
yoga-user <m.rzezutka@gmail.com>
2020-03-30 00:34:04 +0200

    Homework 1

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -3,8 +3,23 @@ package sgh;
 public class PrimeNumbers {
 
     public static void primes(int n) {
-        // Your code here
-        System.out.println("2, 3, 5, 7");
+        int i=0;
+        int num = 0;
+        String primeNumbers = "";
+
+        for (i=2; i<10; i++){
+            int counter = 0;
+            for (num=2; num>=2; num--){
+                if (i%num==0){
+                    counter=counter+1;
+                }
+            }
+            if (counter ==2){
+                primeNumbers= primeNumbers + i +" ";
+            }
+
+        }
+        System.out.println(primeNumbers);
     }
 
 
